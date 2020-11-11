b861e22d70b76c0ae984eaf8ef230e0888693ba5
Gio <canada.gio@gmail.com>
2020-11-10 20:23:32 -0700

    Minor changes to the platforms and disappearing walls

--- Assets/Scripts/ToggleVisible.cs
+++ Assets/Scripts/ToggleVisible.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class ToggleVisible : IsActive
 {
-
+    private bool alreadyDisappeared;
     // Start is called before the first frame update
     void Start()
     {
-        
+        alreadyDisappeared = false;
     }
 
     // Update is called once per frame
     void Update() {
         if (state) {
             //Debug.Log("dissapear");
-            transform.GetComponent<Renderer>().enabled = false;
-            transform.GetComponent<Collider>().enabled = false;
+            
+                transform.GetComponent<Renderer>().enabled = false;
+                transform.GetComponent<Collider>().enabled = false;
+                alreadyDisappeared = true;
+            
         } else {
-            transform.GetComponent<Renderer>().enabled = true;
-            transform.GetComponent<Collider>().enabled = true;
+
+            if (!alreadyDisappeared)
+            {
+                transform.GetComponent<Renderer>().enabled = true;
+                transform.GetComponent<Collider>().enabled = true;
+            }
         }
     }
 
