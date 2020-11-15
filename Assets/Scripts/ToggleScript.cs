601d78c863964a865dc296de21c1601c4269cda9
unknown <acjparadis@outlook.com>
2020-11-14 23:11:54 -0700

    Swapping also swaps checkpoints

--- Assets/Scripts/ToggleScript.cs
+++ Assets/Scripts/ToggleScript.cs
@@ -15,14 +15,32 @@ public class ToggleScript : MonoBehaviour {
 
     [SerializeField]
     private Transform[] targeted;
+	
+	private kill_player[] kp;
+	private GameObject[] spawn_points;
 
     private bool toggle;
 
     private float distance;
+	
+	public void Start() {
+		kp = GameObject.FindObjectsOfType<kill_player>();
+		spawn_points = new GameObject[kp.Length];
+		for (int i=0;i<kp.Length;i++){
+			spawn_points[i] = kp[i].spawn_point;
+		}
+	}
+	
     public void ToggleButton() {
         //Debug.Log(playerDistance());
         if(playerDistance() < max_distance){
            // Debug.Log("toggle");
+			GameObject temp;
+			for (int i=0;i<kp.Length;i++){
+				temp = spawn_points[i];
+				spawn_points[i] = kp[i].spawn_point;
+				kp[i].spawn_point = temp;
+			}
             toggle = !toggle;
             for (int i = 0; i < targeted.Length; i++) {
                // Debug.Log(i);
